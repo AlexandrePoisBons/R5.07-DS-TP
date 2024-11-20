db8519e7f6284e5a9fb66252983834b53f275299
AlexandrePoisBons <102670442+AlexandrePoisBons@users.noreply.github.com>
2024-11-20 13:28:16 +0000

    Add Q4.java and Q4 workflow

---
+++ Q4.java
@@ -0,0 +1,20 @@
+public class Q4 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Veuillez fournir un seul argument : le nombre de lignes.");
+            return;
+        }
+
+        try {
+            int n = Integer.parseInt(args[0]);
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("L'argument fourni n'est pas un nombre valide.");
+        }
+    }
+}
