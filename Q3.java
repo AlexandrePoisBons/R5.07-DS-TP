94b9c1316e621e6d38a43c2e7af8fdb644395f01
AlexandrePoisBons <102670442+AlexandrePoisBons@users.noreply.github.com>
2024-11-20 13:22:47 +0000

    Add Q3.java

--- Q3.java
+++ Q3.java
@@ -1,5 +1,6 @@
 public class Q3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         if (args.length != 1) {
             System.out.println("Veuillez fournir un seul argument : le nombre d'étoiles.");
             return;
